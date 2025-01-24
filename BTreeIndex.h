4d37d5ec3ab23ce2b5f4bbb9939cc731a7350c21
YassenAli <144853147+YassenAli@users.noreply.github.com>
2025-01-24 21:35:26 +0200

    solve some problems

--- BTreeIndex.h
+++ BTreeIndex.h
@@ -3,34 +3,36 @@
 
 #include <fstream>
 #include <vector>
+using namespace std;
 
 class BTreeIndex {
 private:
     struct Node {
         int is_leaf;
         int next_free;
-        vector<int> keys;
-        vector<int> refs;
+        std::vector<int> keys;
+        std::vector<int> refs;
 
-        Node(int m) : keys(m, -1), refs(m, -1) {}
+        Node() : keys(BTreeIndex::getM(), -1),
+                      refs(BTreeIndex::getM(), -1) {}
     };
 
     static int m;
 
     // Helper functions
-    static int getM();
-    static void setM(const int &m);
-    static void readNode(std::fstream& file, int index, Node& node, int m);
-    static void writeNode(std::fstream& file, int index, Node& node, int m);
-    static int allocateFreeNode(std::fstream& file, int m);
-    static void freeNode(std::fstream& file, int index, int m);
-    static void split(std::fstream& file, int nodeIndex, int parentIndex, int m);
-    static void merge(std::fstream& file, int nodeIndex, int parentIndex, int m);
-    static int findKeyIndex(const Node& node, int RecordID, int m);
-    static int searchInNode(std::fstream& file, int nodeIndex, int RecordID, int m);
-    static void updateParent(std::fstream& file, int parentIndex, int newKey, int newRef, int m);
+    static void readNode(fstream& file, int index, Node& node);
+    static void writeNode(fstream& file, int index, Node& node);
+    static int allocateFreeNode(fstream& file);
+    static void freeNode(fstream& file, int index);
+    static void split(fstream& file, int nodeIndex, int parentIndex);
+    static void merge(fstream& file, int nodeIndex, int parentIndex);
+    static int findKeyIndex(const Node& node, int RecordID);
+    static int searchInNode(fstream& file, int nodeIndex, int RecordID);
+    static void updateParent(fstream& file, int parentIndex, int newKey, int newRef);
 
 public:
+    static void setM(int order);
+    static int getM();
     static void CreateIndexFileFile(const char* filename, int numberOfRecords, int m);
     static int InsertNewRecordAtIndex(const char* filename, int RecordID, int Reference);
     static void DeleteRecordFromIndex(const char* filename, int RecordID);
