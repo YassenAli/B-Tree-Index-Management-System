f2cbfe0b337a918cd403dfb85f689243957a59fa
MalakGamil <malakgami098@gmail.com>
2025-01-25 12:47:14 +0200

    insert,delete

--- BTreeIndex.h
+++ BTreeIndex.h
@@ -1,51 +1,64 @@
-#ifndef B_TREE_INDEX_MANAGEMENT_SYSTEM_BTREEINDEX_H
-#define B_TREE_INDEX_MANAGEMENT_SYSTEM_BTREEINDEX_H
+#ifndef BTREEINDEX_BTREEINDEX_H
+#define BTREEINDEX_BTREEINDEX_H
 
+#include <iostream>
+#include <string>
 #include <fstream>
 #include <vector>
+#include <stack>
+#include <utility>
+#include <sstream>
+#include <algorithm>
+#include <tuple>
 using namespace std;
 
-class BTreeIndex {
-private:
-    struct Node {
-        int is_leaf;
-        int next_free;
-        vector<int> keys;
-        vector<int> refs;
-
-        Node() : keys(BTreeIndex::getM(), -1),
-                      refs(BTreeIndex::getM(), -1) {}
-    };
-
-    static int m;
-
-    // Helper functions
-    static void readNode(fstream& file, int index, Node& node);
-    static void writeNode(fstream& file, int index, Node& node);
-    static int allocateFreeNode(fstream& file);
-    static void freeNode(fstream& file, int index);
-    static void split(fstream& file, int nodeIndex, int parentIndex);
-    static void merge(fstream& file, int nodeIndex, int parentIndex);
-    static int findKeyIndex(const Node& node, int RecordID);
-    static int searchInNode(fstream& file, int nodeIndex, int RecordID);
-    static void updateParent(fstream& file, int parentIndex, int newKey, int newRef);
-
+struct BTreeNode
+{
 public:
-    static void setM(int order);
-    static int getM();
-    static void CreateIndexFileFile(const char* filename, int numberOfRecords, int m);
-    static int InsertNewRecordAtIndex(const char* filename, int RecordID, int Reference);
-    void DeleteRecordFromIndex(const char* filename, int RecordID);
-    static void DisplayIndexFileContent(const char* filename);
-    static int SearchARecord(const char* filename, int RecordID);
-
-    void handleUnderflow(fstream &file, int nodeIndex, vector<pair<int, int>> &parentStack);
-
-    bool canRedistribute(const Node &node);
-
-    void redistributeFromLeft(fstream &file, int nodeIndex, int leftSiblingIndex, int parentIndex, int keyPos);
-
-    void mergeWithLeft(fstream &file, int nodeIndex, int parentIndex, int keyPos);
+    int isLeaf;
+    // update
+    int count = 0;
+    int place;
+    vector<pair<int, int>> node;
+    vector<BTreeNode> children;
 };
 
-#endif //B_TREE_INDEX_MANAGEMENT_SYSTEM_BTREEINDEX_H
+class BTreeIndex
+{
+
+    const char *const BTreeFileName = "BTreeIndex.txt";
+    int numberOfRecords;
+    int m;
+    int head{};
+    fstream BTreeFile;
+    void DeleteCase2(const char *filename, vector<BTreeNode> &bTree, BTreeNode &find, int RecordID, int &count, int &temp);
+    void DeleteCase1(const char *filename, vector<BTreeNode> &bTree, BTreeNode &find, int RecordID);
+
+
+public:
+    void CreateIndexFile(const char *filename, int numberOfRecords, int m);
+    int InsertNewRecordAtIndex(int RecordID, int Reference);
+    void DeleteRecordFromIndex(const char *filename, int RecordID, int m);
+    void DisplayIndexFileContent(const char *filename);
+    int SearchARecord(const char *filename, int RecordID);
+    void run();
+
+    //////////////////////////////////////Functions for searching//////////////////////////////////////
+    bool record_valid(int recordNumber) const;
+    int read_val(int rowIndex, int columnIndex);
+    bool isEmpty(int recordNumber);
+    bool isLeaf(int recordNumber);
+    vector<pair<int, int>> read_node_values(int recordNumber);
+
+    vector<BTreeNode> readFile(const char *filename);
+    void savefile(const char *filename, vector<BTreeNode> bTree, int m);
+
+
+    /////////////////////////////functions for insert////////////////////////////////////////////
+    int split(int i,vector<BTreeNode> bTree);
+    bool split_root(vector<BTreeNode> bTree);
+    pair<vector<pair<int, int>>, vector<pair<int, int>>> splitOriginalNode(const vector<pair<int, int>>& originalNode);
+    int updateAfterInsert(int parentRecordNumber, int newChildRecordNumber);
+};
+
+#endif // BTREEINDEX_BTREEINDEX_H
